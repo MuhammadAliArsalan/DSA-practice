2b3451da4852f92b4a3b05fd54aa625265ed5cec
Ali Arslan <arsalanli873@gmail.com>
2025-03-18 13:46:08 +0500

    2 problems solved

--- arrays/twoSum.js
+++ arrays/twoSum.js
@@ -1,18 +1,13 @@
-
-var twoSum = function(nums, target) {   //better and simpler appraoch
-    
-    for(i=0;i<nums.length;i++){
-        for(j=i+1;j<nums.length;j++){
+var twoSum = function(nums, target) {
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
             if(nums[i]+nums[j]==target){
                 return [i,j]
             }
-            
         }
-    
-        }
-        return []
+    }   
 };
-console.log(twoSum([1,8,28,15,7,12],22))
+console.log(twoSum([2,7,11,15],18))
 
 // var twoSum = function(nums, target) {
 //     let sum=0;
